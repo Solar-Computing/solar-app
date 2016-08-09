db55078970925fe0fa8ce2541d6c8507f2d88be2
Dhruv Mehra <dmehra6@gatech.edu>
2016-08-08 21:18:50 -0400

    secondary banner

--- public/app/src/components/primary-banner.component.ts
+++ public/app/src/components/primary-banner.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SecondaryBannerComponent } from './secondary-banner.component';
 
 @Component({
   selector: 'banner-primary',
   templateUrl: 'app/templates/primary-banner.component.html',
-  styleUrls: ['app/styles/css/primary-banner.component.css']
+  styleUrls: ['app/styles/css/primary-banner.component.css'],
+  directives: [SecondaryBannerComponent]
 })
 
 export class PrimaryBannerComponent { }
---
+++ public/app/src/components/secondary-banner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'banner-secondary',
+  templateUrl: 'app/templates/secondary-banner.component.html',
+  styleUrls: ['app/styles/css/secondary-banner.component.css']
+})
+
+export class SecondaryBannerComponent { }
