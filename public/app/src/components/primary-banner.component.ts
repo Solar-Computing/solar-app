bc038700d27df70bb8b023cd471d2e7df767dac3
Dhruv Mehra <dmehra6@gatech.edu>
2016-08-08 21:57:49 -0400

    tertiary-banner

--- public/app/src/components/primary-banner.component.ts
+++ public/app/src/components/primary-banner.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SecondaryBannerComponent } from './secondary-banner.component';
+import { TertiaryBannerComponent } from './tertiary-banner.component';
 
 @Component({
   selector: 'banner-primary',
   templateUrl: 'app/templates/primary-banner.component.html',
   styleUrls: ['app/styles/css/primary-banner.component.css'],
-  directives: [SecondaryBannerComponent]
+  directives: [SecondaryBannerComponent, TertiaryBannerComponent]
 })
 
 export class PrimaryBannerComponent { }
---
+++ public/app/src/components/tertiary-banner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'banner-tertiary',
+  templateUrl: 'app/templates/tertiary-banner.component.html',
+  styleUrls: ['app/styles/css/tertiary-banner.component.css']
+})
+
+export class TertiaryBannerComponent { }
