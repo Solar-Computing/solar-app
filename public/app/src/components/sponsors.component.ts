957cf156bbe616d2332916b68904d9c716929eb8
Dhruv Mehra <dmehra6@gatech.edu>
2016-08-11 17:16:55 -0400

    add sponsors

---
+++ public/app/src/components/sponsors.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sponsor',
+  // styleUrls: ['app/styles/css/sponsors.component.css']
+  templateUrl: 'app/templates/sponsors.component.html',
+})
+
+export class SponsorsComponent { }
