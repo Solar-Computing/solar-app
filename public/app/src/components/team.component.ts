6ff19c5b93ebd01d178221a9e35ea492e81e21ed
Dhruv Mehra <dmehra6@gatech.edu>
2016-08-19 20:17:12 -0400

    finish the team picture

--- public/app/src/components/team.component.ts
+++ public/app/src/components/team.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ImageBannerComponent } from './image-banner.component';
+
 @Component({
   selector: 'team',
   templateUrl: 'app/templates/team.component.html',
-  styleUrls: ['app/styles/css/team.component.css']
+  styleUrls: ['app/styles/css/team.component.css'],
+  directives: [ImageBannerComponent]
 })
 
 export class TeamComponent { }
