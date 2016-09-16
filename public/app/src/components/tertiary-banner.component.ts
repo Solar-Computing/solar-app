4cdbeba5a5713328e95495ca27b171053bdae630
Dhruv Mehra <dmehra6@gatech.edu>
2016-09-15 23:09:41 -0400

    hide and show banner

--- public/app/src/components/tertiary-banner.component.ts
+++ public/app/src/components/tertiary-banner.component.ts
@@ -10,6 +10,7 @@ export class TertiaryBannerComponent {
   @Input() title: string;
   @Input() description: string;
   @Input() text: string = "Lorem ipsum dolor sit amet, consectetur adipisici elit, \nsed eiusmod tempor incidunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud";
+  @Input() toggle: boolean = true;
 
   show_text: boolean;
 
