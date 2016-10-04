0ed83900ff10857cbb4ecf3220f77ea9ed320aa9
Dhruv Mehra <dmehra6@gatech.edu>
2016-10-04 10:37:31 -0400

    add hello api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # devise_for :users
 
-  get "/simulation/" => "simulations#get_simulation"
-  get "/simulation/daily" => "simulations#get_daily_load"
+  get "api" => 'apis#say_hello'
+  get "simulation" => "simulations#get_simulation"
+  get "simulation/daily" => "simulations#get_daily_load"
 end
