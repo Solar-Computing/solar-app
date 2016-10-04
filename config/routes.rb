41b5b63453aa3636559e6a86ae00433805bd049f
Dhruv Mehra <dmehra6@gatech.edu>
2016-10-04 10:26:49 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # devise_for :users
 
-  get "/api/simulation/" => "simulations#get_simulation"
-  get "/api/simulation/daily" => "simulations#get_daily_load"
+  get "/simulation/" => "simulations#get_simulation"
+  get "/simulation/daily" => "simulations#get_daily_load"
 end
